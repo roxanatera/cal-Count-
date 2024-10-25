9d7604def9757843aa96b14975f27984ecc513b4
Julia Roxana Natera <103115280+roxanatera@users.noreply.github.com>
2024-10-25 17:06:11 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useMemo } from "react"
+import Form from "./components/Form"
+import ActivityList from "./components/ActivityList"
+import CalorieTracker from "./components/CalorieTracker"
+import { useActivity } from "./hooks/useActivity"
+
+function App() {
+  const { state, dispatch } = useActivity()
+
+  useEffect(() => {
+    localStorage.setItem("activities", JSON.stringify(state.activities))
+  }, [state.activities])
+
+  const canRestartApp = () => useMemo(() => state.activities.length, [state.activities])
+
+  return (
+    <>
+      <header className="bg-emerald-600 py-3">
+        <div className="max-w-4xl mx-auto italic flex justify-between items-center">
+          <h1 className="text-center text-lg font-bold text-white uppercase">
+            cal-count+
+          </h1>
+          <button
+            className="bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm disabled:opacity-10"
+            disabled={!canRestartApp()}
+            onClick={() => dispatch({ type: "restart-app" })}
+          >
+            Reiniciar App
+          </button>
+        </div>
+      </header>
+
+      <section className="bg-emerald-500 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+          <Form />
+        </div>
+      </section>
+
+      <section className="bg-emerald-600 py-10">
+        <div className="max-w-4xl mx-auto">
+          <CalorieTracker />
+        </div>
+      </section>
+
+      <section className="p-10 mx-auto max-w-4xl">
+        <ActivityList />
+      </section>
+    </>
+  )
+}
+
+export default App
